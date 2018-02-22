cabf53b62d2e51ac655bf405e01977104f72a00d
blfuentes <epiphonetodie@gmail.com>
2018-02-22 16:38:44 +0100

    insertone for test case

--- TestMongoDB/app.js
+++ TestMongoDB/app.js
@@ -5,12 +5,12 @@ const assert = require('assert');
 const url = 'mongodb://localhost:27017/';
 
 // Database Name
-// const dbName = 'accountingmanager';
-const dbName = 'nodetest1';
+const dbName = 'accountingmanager';
+// const dbName = 'nodetest1';
 
 // Database Collection
-// const collection = 'annotation';
-const collectionName = 'usercollection';
+const collectionName = 'annotation';
+// const collectionName = 'usercollection';
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, client) {
@@ -20,31 +20,35 @@ MongoClient.connect(url, function(err, client) {
     const collection = db.collection(collectionName);
     console.log('first element: '+collection.find()[0]);
 
-    createCapped(db, function() {
-        var collection2 = db.collection("myCollection");
-        // db.collection('myCollection').insertOne({myid:1}, function(err, r) {
-        //     assert.equal(null, err);
-        //     // assert.equal(1, r.insertedCount);
-        //     console.log('first element: '+db.collection('myCollection').findOne().myid);
-        //     client.close();
-        // });
-        collection2.insertOne({myid:2}, function(err, r) {
-            assert.equal(null, err);
-            // assert.equal(2, r.insertedCount);
-            console.log('first element: '+collection2.findOne().myid);
-            client.close();
-        });
-      });
+    collection.insertOne({name:"test", type: 2, date: new Date(), amount: 10}, function(err, r){
+        client.close();
+    });
+
+    // createCapped(db, function() {
+    //     var collection2 = db.collection("myCollection");
+    //     // db.collection('myCollection').insertOne({myid:1}, function(err, r) {
+    //     //     assert.equal(null, err);
+    //     //     // assert.equal(1, r.insertedCount);
+    //     //     console.log('first element: '+db.collection('myCollection').findOne().myid);
+    //     //     client.close();
+    //     // });
+    //     collection2.insertOne({myid:2}, function(err, r) {
+    //         assert.equal(null, err);
+    //         // assert.equal(2, r.insertedCount);
+    //         console.log('first element: '+collection2.findOne().myid);
+    //         client.close();
+    //     });
+    //   });
 });
 
-function createCapped(db, callback) {
-    db.createCollection("myCollection", { "capped": true, "size": 100000, "max": 5000},
-      function(err, results) {
-        console.log("Collection created.");
-        callback();
-      }
-    );
-};
+// function createCapped(db, callback) {
+//     db.createCollection("myCollection", { "capped": true, "size": 100000, "max": 5000},
+//       function(err, results) {
+//         console.log("Collection created.");
+//         callback();
+//       }
+//     );
+// };
 
 // mongoose !!! THIS WORKS!
 ///////////////////////////////////////////////////////////////////////////
