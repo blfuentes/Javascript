9f0e9845a2b311a16e6c0783da948dd5ee95cce2
blfuentes <epiphonetodie@gmail.com>
2018-02-24 19:52:42 +0100

    -

--- AccountingManager/routes/users.js
+++ AccountingManager/routes/users.js
@@ -6,7 +6,7 @@ var db = require('../db');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   db.getCollection('annotation', function(items){
-    console.log('First element: ' + items[0].name);
+    
     res.send('respond with a resource!');
   });
 });
